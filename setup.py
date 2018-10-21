d606e28593743ddf7687a2961d0a719f7f1b49e9
Ville Brofeldt <ville.brofeldt@iki.fi>
2018-10-21 16:26:18 +0300

    Add python versions and license to setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,17 @@
 from setuptools import setup
 
-setup(name='gym_yahtzee',
-      version='0.0.1',
-      install_requires=[
-          'gym',
-          'pip-tools'
-      ]
+setup(
+    name='gym_yahtzee',
+    version='0.0.1',
+    install_requires=[
+        'gym',
+        'pip-tools',
+    ],
+    classifiers = [
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+    ],
 )
