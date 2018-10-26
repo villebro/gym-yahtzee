49255734d96ed839c973815a4216bb85b9e584fc
Ville Brofeldt <ville.brofeldt@iki.fi>
2018-10-26 07:14:17 +0300

    Add additional info to setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,17 @@
-from setuptools import setup
+from setuptools import find_packages, setup
+
 
 setup(
     name='gym_yahtzee',
     version='0.9.0',
+    description='Yahtzee game engine for OpenAI Gym',
+    author='Ville Brofeldt',
+    author_email='ville.brofeldt@iki.fi',
+    maintainer='Ville Brofeldt',
+    maintainer_email='ville.brofeldt@iki.fi',
+    url='https://github.com/villebro/gym-yahtzee',
+    license='MIT',
+    packages=find_packages(exclude='tests'),
     install_requires=[
         'gym',
     ],
